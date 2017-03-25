801ff35be4a0df732a16efbcff0233024e583d41
Tim Kohn <kohnx074@umn.edu>
2017-03-25 17:38:31 -0500

    Change to express

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+var express = require('express');
+var app = express();
 const { AkairoClient } = require('discord-akairo');
 
 const token = process.env.DISCTOKEN;
@@ -10,6 +12,22 @@ const client = new AkairoClient({
     listenerDirectory: './listeners/'
 });
 
-client.login(token).then(() => {
-    console.log('Ready!');
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+//
+// // views is directory for all template files
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.send('hi');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+  client.login(token).then(() => {
+      console.log('Ready!');
+  });
 });
