2b048a74b327366d323e7def62257d18fc9e98b5
Tim Kohn <kohnx074@umn.edu>
2017-03-25 20:28:19 -0500

    Adding ping timer to keep bot alive

--- app.js
+++ app.js
@@ -1,25 +1,20 @@
 var express = require('express');
 var app = express();
 const { AkairoClient } = require('discord-akairo');
+var http = require("http");
 
 const token = process.env.DISCTOKEN;
 
 const client = new AkairoClient({
-    ownerID: '123992700587343872',
+    ownerID: '295047872183795714',
     prefix: '!',
     commandDirectory: './commands/',
     inhibitorDirectory: './commands/',
     listenerDirectory: './listeners/'
 });
 
-
 app.set('port', (process.env.PORT || 5000));
-
 app.use(express.static(__dirname + '/public'));
-//
-// // views is directory for all template files
-// app.set('views', __dirname + '/views');
-// app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.send('hi');
@@ -29,5 +24,8 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
   client.login(token).then(() => {
       console.log('Ready!');
+      setInterval(function() {
+          http.get("http://zinquoter.herokuapp.com");
+      }, 300000); // every 5 minutes (300000)
   });
 });
